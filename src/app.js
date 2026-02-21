81d73b299f84ffcc54ef1b50db85257995a8c1a2
Nikhil07 <nikhil.nayak4561@gmail.com>
2026-02-21 15:01:11 +0530

    feat: mount auth routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
@@ -8,6 +9,8 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 // health route
 app.get("/health", (req, res) => {
