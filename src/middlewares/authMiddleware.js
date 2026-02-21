617671d6a6c215c539cb58fee18a8830b06c4957
Nikhil07 <nikhil.nayak4561@gmail.com>
2026-02-21 15:30:47 +0530

    feat: add role-based authorization middleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -34,3 +34,16 @@ export const protect = async (req, res, next) => {
     });
   }
 };
+
+export const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({
+        success: false,
+        message: "Forbidden: insufficient permissions"
+      });
+    }
+
+    next();
+  };
+};
