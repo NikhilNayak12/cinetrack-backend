0df68824cd77230688ade406d7e745d4230cff9d
Nikhil07 <nikhil.nayak4561@gmail.com>
2026-02-21 15:32:52 +0530

    feat: mount user routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(helmet());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 // health route
 app.get("/health", (req, res) => {
