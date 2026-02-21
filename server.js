c4112b91f1b8eeb18142cef0cec33a13bebeccfe
Nikhil07 <nikhil.nayak4561@gmail.com>
2026-02-21 14:46:25 +0530

    feat: integrate mongodb connection in server

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
+import dotenv from "dotenv";
 import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
+
+dotenv.config();
+
+// connect database
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
